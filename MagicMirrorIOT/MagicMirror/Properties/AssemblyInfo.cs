57167a45e5061c28b4dec1c8c586ad9c07739b2b
Cristina Valerevna <cristina.v.tapes@gmail.com>
2017-11-05 22:10:00 +0200

    Rename project to MagicMirror and solution to MagicMirrorIOT

--- MagicMirrorIOT/MagicMirror/Properties/AssemblyInfo.cs
+++ MagicMirrorIOT/MagicMirror/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FirstIOT")]
+[assembly: AssemblyTitle("MagicMirror")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FirstIOT")]
+[assembly: AssemblyProduct("MagicMirror")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
